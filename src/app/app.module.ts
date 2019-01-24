be0b3e84cdbd815e80cef2ca23876b310041bf9d
darshan1234 <darshanmakvana@gmail.com>
2019-01-24 14:50:56 -0600

    commit v 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header/header.component';
+import { InstructorComponent } from './instructor/instructor.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRouteModule} from './app-route/app-route.module';
+import { InstructorAddComponent } from './instructor/instructor-add/instructor-add.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CourseAddComponent } from './course/course-add/course-add.component';
+import { InstructorHeaderComponent } from './header/instructor-header/instructor-header.component';
 
+import { CourseViewComponent } from './course/course-view/course-view.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InstructorComponent,
+    InstructorAddComponent,
+    CourseAddComponent,
+    InstructorHeaderComponent,
+
+    CourseViewComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule, NgbModule, HttpClientModule, AppRouteModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
